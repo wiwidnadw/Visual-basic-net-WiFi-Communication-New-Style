ffe892b368b423b4f035d80fe2378f8dc349488e
wiwidnadw <63993989+wiwidnadw@users.noreply.github.com>
2022-02-15 07:40:37 +0700

    Update esp32_vbnet_wificonnection.ino

--- esp32_vbnet_wificonnection.ino
+++ esp32_vbnet_wificonnection.ino
@@ -1,3 +1,12 @@
+// create by Nur Ahmad
+//github.com/wiwidnadw
+//Youtube channel : Digitalneering
+//video tutorial for this project : https://youtu.be/jn_oRLLJz64
+
+//Support my platform on github and Youtube by Like, share and subs ! Enjoy
+
+
+
 #include <WiFi.h>
 
 //IPAddress ip(192, 168, 43, 150);
